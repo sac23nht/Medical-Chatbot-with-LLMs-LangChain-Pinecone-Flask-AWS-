505eabdb285dcabd7405923cd8a088c0680b07a0
Saijaya <Saijaya.Chilekampalli.nht@fh-zwickau.de>
2025-08-21 13:14:41 +0200

    folderes structure added

---
+++ template.sh
@@ -0,0 +1,15 @@
+mkdir -p src
+mkdir -p research
+
+
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trials.ipynb
+touch requirments.txt
+
+
+echo "Directory files created successfully."
