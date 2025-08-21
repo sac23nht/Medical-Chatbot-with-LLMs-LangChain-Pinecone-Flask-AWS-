c513cc289e8517827c8edb24d2b866b7cdaa8c0b
Saijaya <Saijaya.Chilekampalli.nht@fh-zwickau.de>
2025-08-22 01:03:06 +0200

    vector DB Created

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Saijaya Chilekampalli",
+    author_email="saijaya.c@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
